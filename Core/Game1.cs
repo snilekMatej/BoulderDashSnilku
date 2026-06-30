22e1098b26240c40522cd9c89fa499a0d7547fc0
snilekMatej <snilekmatej@gmail.com>
2026-06-30 02:02:54 +0200

    Creating architecture

--- Core/Game1.cs
+++ Core/Game1.cs
@@ -2,7 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace BoulderDashSnilku;
+namespace BoulderDashSnilku.Core;
 
 public class Game1 : Game
 {
--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new BoulderDashSnilku.Game1();
+﻿using var game = new BoulderDashSnilku.Core.Game1();
 game.Run();
